de044642986b9a2da32098438effee0b9ebbcf78
Grant Tepper <grant.tepper@gmail.com>
2015-04-05 17:47:10 -0400

    implements structs roughtly off the model

--- goqueen.go
+++ goqueen.go
@@ -5,7 +5,39 @@ import (
 	mw "github.com/labstack/echo/middleware"
 	"github.com/rs/cors"
 	"github.com/thoas/stats"
+	"net/http"
 )
+
+type card struct {
+	ID        int    `json:"id"`
+	Name      string `json:"name"`
+	Code      string `json:"code"`
+	Pin       string `json:"pin"`
+	IsActive  bool   `json:"is_active"`
+	CreatedAt string `json:"created_at"`
+	UpdatedAt string `json:"updated_at"`
+}
+
+type log struct {
+	ID        int    `json:"id"`
+	Code      string `json:"code"`
+	ValidPin  string `json:"valid_pin"`
+	CreatedAt string `json:"created_at"`
+}
+
+type scheudle struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+	Mon  int    `json:"mon"`
+	Tue  int    `json:"tue"`
+	Wed  int    `json:"wed"`
+	Thu  int    `json:"thu"`
+	Fri  int    `json:"fri"`
+	Sat  int    `json:"sat"`
+	Sun  int    `json:"sun"`
+}
+
+var cards map[string]card
 
 func main() {
 	e := echo.New()
