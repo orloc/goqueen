12d257448e68c2ef933e78a6798d9de63905810e
Grant Tepper <grant.tepper@gmail.com>
2015-04-08 01:08:41 -0400

    fixes benchmark-api js

---
+++ public/benchamrk.js
@@ -0,0 +1,61 @@
+(function(){
+
+    var apiBenchmark = require('api-benchmark');
+     
+    var service = {
+      goqueenapi: "http://localhost:8080/"
+    };
+
+    var options = { 
+        debug: true,
+        minSamples: 1000, 
+        stopOnError: false,
+        maxTime: 40,
+        runMode: 'parallel',
+        maxConcurrentRequests: 500,
+    };
+     
+    var routes = { 
+        getSchedules: {
+            method: 'get', 
+            route: 'api/schedules',
+        },
+        postSchedules: {
+            method: 'post',
+            route: 'api/schedules',
+            data: getScheduleData()
+        },
+        stats: {
+            method: 'get',
+            route: 'stats' 
+        }
+    };
+    var start = new Date().getTime();
+
+
+
+    apiBenchmark.compare(service, routes, options, function(err, results){
+      console.log(results, err);
+    var end = new Date().getTime();
+    var time = end - start;
+
+    console.log(time/100)
+      // displays some stats! 
+    });
+
+    function getScheduleData(){
+        return {
+            name:"dsfadf",
+            mon:false,
+            tue:false,
+            wed:true,
+            thu:false,
+            fri:true,
+            sat:true,
+            sun:false,
+            startTime: 1431403200,
+            endTime:1431489480 
+        };
+    }
+
+})()
